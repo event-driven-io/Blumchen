59673b29880f96007c87fda7c824de768c83c7ca
giordanol <luca.giordano@ynap.com>
2024-07-01 09:00:32 +0200

    mark CreatePublication internal to expose to testing suite

--- src/Tests/When_Subscription_Already_Exists.cs
+++ src/Tests/When_Subscription_Already_Exists.cs
@@ -1,11 +1,9 @@
-using System.Diagnostics;
-using Blumchen.Database;
 using Blumchen.Publications;
 using Blumchen.Serialization;
 using Blumchen.Subscriptions;
+using Blumchen.Subscriptions.Management;
 using Blumchen.Subscriptions.ReplicationMessageHandlers;
 using Npgsql;
-using Npgsql.Replication;
 using Xunit.Abstractions;
 
 namespace Tests;
@@ -19,14 +17,17 @@ public class When_Subscription_Already_Exists(ITestOutputHelper testOutputHelper
         var ct = TimeoutTokenSource().Token;
         var sharedNamingPolicy = new AttributeNamingPolicy();
         var connectionString = Container.GetConnectionString();
-        var eventsTable = await CreateOutboxTable(NpgsqlDataSource.Create(connectionString), ct);
+        var dataSource = NpgsqlDataSource.Create(connectionString);
+        var eventsTable = await CreateOutboxTable(dataSource, ct);
         var publisherResolver = new PublisherSetupOptionsBuilder()
             .JsonContext(PublisherContext.Default)
             .NamingPolicy(sharedNamingPolicy)
             .Build();
         var slotName = "subscription_test";
         var publicationName = "publication_test";
-        await SetupReplication(connectionString, slotName, publicationName, eventsTable, ct);
+
+
+        await dataSource.CreatePublication(publicationName, eventsTable, new HashSet<string>{"urn:message:user-created:v1"}, ct);
 
 
         var ( _, subscriptionOptions) = SetupFor<SubscriberUserCreated>(connectionString, eventsTable,
@@ -47,25 +48,5 @@ public class When_Subscription_Already_Exists(ITestOutputHelper testOutputHelper
             Assert.Equal(@expected, ((OkEnvelope)envelope).Value);
             return;
         }
-    }
-
-    private static async Task SetupReplication(
-        string connectionString,
-        string slotName,
-        string publicationName,
-        string tableName,
-        CancellationToken ct)
-    {
-        var dataSource = NpgsqlDataSource.Create(connectionString);
-        await using var source = dataSource.ConfigureAwait(false);
-        await dataSource.Execute($"CREATE PUBLICATION {publicationName} FOR TABLE {tableName} WITH (publish = 'insert');", ct).ConfigureAwait(false);
-        var connection = new LogicalReplicationConnection(connectionString);
-        await using var connection1 = connection.ConfigureAwait(false);
-        await connection.Open(ct).ConfigureAwait(false);
-        await connection.CreatePgOutputReplicationSlot(
-            slotName,
-            slotSnapshotInitMode: LogicalSlotSnapshotInitMode.Export,
-            cancellationToken: ct
-        ).ConfigureAwait(false);
     }
 }
