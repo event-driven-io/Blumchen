9d3249a0c39139478dfa0122ecbbc3a77b24e4e1
giordanol <luca.giordano@ynap.com>
2024-07-06 14:24:55 +0200

    file renamed

--- src/Blumchen/Serialization/JsonTypeResolverExtensions.cs
+++ src/Blumchen/Serialization/JsonTypeResolverExtensions.cs
@@ -1,7 +1,7 @@
 namespace Blumchen.Serialization;
 
 
-public static class DictionaryExtensions
+public static class JsonTypeResolverExtensions
 {
     internal static IEnumerable<string> Keys(this JsonTypeResolver? resolver) => resolver?.RegisteredTypes.Keys ?? Enumerable.Empty<string>();
     internal static IEnumerable<Type> Values(this JsonTypeResolver? resolver) => resolver?.RegisteredTypes.Values ?? Enumerable.Empty<Type>();
