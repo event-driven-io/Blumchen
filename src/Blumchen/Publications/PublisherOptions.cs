e71999b06e453ce8831aa931640e64aa7a80af77
giordanol <luca.giordano@ynap.com>
2024-07-17 10:45:19 +0200

    Expose shortcut for  table validation when bootstrapping publisher

--- src/Blumchen/Publications/PublisherOptions.cs
+++ src/Blumchen/Publications/PublisherOptions.cs
@@ -1,5 +1,20 @@
+using Blumchen.Database;
 using Blumchen.Serialization;
+using Npgsql;
 
 namespace Blumchen.Publications;
 
 public record PublisherOptions(TableDescriptorBuilder.MessageTable TableDescriptor, IJsonTypeResolver JsonTypeResolver);
+
+public static class PublisherOptionsExtensions
+{
+    public static async Task<PublisherOptions> EnsureTable(this PublisherOptions publisherOptions, NpgsqlDataSource dataSource, CancellationToken ct)
+    {
+        await dataSource.EnsureTableExists(publisherOptions.TableDescriptor, ct);
+        return publisherOptions;
+    }
+
+    public static Task<PublisherOptions> EnsureTable(this PublisherOptions publisherOptions,
+        string connectionString, CancellationToken ct)
+        => EnsureTable(publisherOptions, new NpgsqlDataSourceBuilder(connectionString).Build(), ct);
+}
