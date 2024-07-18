06bd0a11adfb4f57db1085b80700128982d4db72
giordanol <luca.giordano@ynap.com>
2024-07-18 16:01:42 +0200

    unused file

--- src/Blumchen/Serialization/JsonTypeResolverExtensions.cs
+++
@@ -1,8 +0,0 @@
-namespace Blumchen.Serialization;
-
-
-public static class JsonTypeResolverExtensions
-{
-    internal static IEnumerable<string> Keys<T>(this ITypeResolver<T>? resolver) => resolver?.RegisteredTypes.Keys ?? Enumerable.Empty<string>();
-    internal static IEnumerable<Type> Values<T>(this ITypeResolver<T>? resolver) => resolver?.RegisteredTypes.Values ?? Enumerable.Empty<Type>();
-}
