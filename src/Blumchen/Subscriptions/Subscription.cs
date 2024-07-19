678b2c81819eb9acf5ba5dba7887b7115dd96ebc
giordanol <luca.giordano@ynap.com>
2024-07-19 18:56:19 +0200

    formatting stuff

--- src/Blumchen/Subscriptions/Subscription.cs
+++ src/Blumchen/Subscriptions/Subscription.cs
@@ -7,17 +7,27 @@ using Npgsql;
 using Npgsql.Replication;
 using Npgsql.Replication.PgOutput;
 using Npgsql.Replication.PgOutput.Messages;
+using static Blumchen.Subscriptions.Management.ReplicationSlotManagement.CreateReplicationSlotResult;
 
 namespace Blumchen.Subscriptions;
 
-using static PublicationManagement;
-using static ReplicationSlotManagement;
-using static ReplicationSlotManagement.CreateReplicationSlotResult;
-
 public sealed class Subscription: IAsyncDisposable
 {
-    private readonly Func<string, IMessageHandler, Type, (IMessageHandler messageHandler, MethodInfo methodInfo)> _memoizer = Memoizer<string, IMessageHandler, Type, (IMessageHandler messageHandler,
-        MethodInfo methodInfo)>.Execute(MessageHandler);
+    private LogicalReplicationConnection? _connection;
+    private readonly SubscriptionOptionsBuilder _builder = new();
+
+    private readonly Func<string, IMessageHandler, Type, (IMessageHandler messageHandler, MethodInfo methodInfo)>
+        _messageHandler = Memoizer<string, IMessageHandler, Type, (IMessageHandler messageHandler,
+            MethodInfo methodInfo)>.Execute(MessageHandler);
+
+    private static (IMessageHandler messageHandler, MethodInfo methodInfo) MessageHandler(
+        string messageType, IMessageHandler messageHandler, Type objType)
+    {
+        var methodInfos = messageHandler.GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public);
+        var methodInfo = methodInfos.SingleOrDefault(mi => mi.GetParameters().Any(pa => pa.ParameterType == objType))
+                         ?? throw new NotSupportedException($"Unregistered type for {objType.AssemblyQualifiedName}");
+        return (messageHandler, methodInfo);
+    }
 
     public enum CreateStyle
     {
@@ -25,10 +35,6 @@ public sealed class Subscription: IAsyncDisposable
         AlwaysRecreate,
         Never
     }
-
-    private LogicalReplicationConnection? _connection;
-
-    private readonly SubscriptionOptionsBuilder _builder = new();
 
     public async IAsyncEnumerable<IEnvelope> Subscribe(
         Func<SubscriptionOptionsBuilder, SubscriptionOptionsBuilder> builder,
@@ -40,18 +46,20 @@ public sealed class Subscription: IAsyncDisposable
     }
 
     internal async IAsyncEnumerable<IEnvelope> Subscribe(
-       ISubscriptionOptions subscriptionOptions,
-       [EnumeratorCancellation] CancellationToken ct = default
-   )
+        ISubscriptionOptions subscriptionOptions,
+        [EnumeratorCancellation] CancellationToken ct = default
+    )
     {
-        var (dataSource, connectionStringBuilder, publicationSetupOptions, replicationSlotSetupOptions, errorProcessor, registry) = subscriptionOptions;
+        var (dataSource, connectionStringBuilder, publicationSetupOptions, replicationSlotSetupOptions, errorProcessor,
+            registry) = subscriptionOptions;
         await dataSource.EnsureTableExists(publicationSetupOptions.TableDescriptor, ct).ConfigureAwait(false);
 
         _connection = new LogicalReplicationConnection(connectionStringBuilder.ConnectionString);
         await _connection.Open(ct).ConfigureAwait(false);
 
         await dataSource.SetupPublication(publicationSetupOptions, ct).ConfigureAwait(false);
-        var result = await dataSource.SetupReplicationSlot(_connection, replicationSlotSetupOptions, ct).ConfigureAwait(false);
+        var result = await dataSource.SetupReplicationSlot(_connection, replicationSlotSetupOptions, ct)
+            .ConfigureAwait(false);
         var replicationDataMapper = new ReplicationDataMapper(registry);
         PgOutputReplicationSlot slot;
 
@@ -68,23 +76,29 @@ public sealed class Subscription: IAsyncDisposable
                 )
             );
 
-            await foreach (var envelope in ReadExistingRowsFromSnapshot(dataSource, created.SnapshotName, replicationDataMapper, publicationSetupOptions.TableDescriptor, publicationSetupOptions.RegisteredTypes,  ct).ConfigureAwait(false))
+            await foreach (var envelope in ReadExistingRowsFromSnapshot(dataSource, created.SnapshotName,
+                               replicationDataMapper, publicationSetupOptions.TableDescriptor,
+                               publicationSetupOptions.RegisteredTypes, ct).ConfigureAwait(false))
             {
-                await foreach (var subscribe in ProcessEnvelope(envelope, registry, errorProcessor).WithCancellation(ct).ConfigureAwait(false))
+                await foreach (var subscribe in ProcessEnvelope(envelope, registry, errorProcessor).WithCancellation(ct)
+                                   .ConfigureAwait(false))
                     yield return subscribe;
             }
         }
 
         await foreach (var message in
                        _connection.StartReplication(slot,
-                           new PgOutputReplicationOptions(publicationSetupOptions.PublicationName, 1, replicationSlotSetupOptions.Binary), ct).ConfigureAwait(false))
+                           new PgOutputReplicationOptions(publicationSetupOptions.PublicationName, 1,
+                               replicationSlotSetupOptions.Binary), ct).ConfigureAwait(false))
         {
             if (message is InsertMessage insertMessage)
             {
                 var envelope = await replicationDataMapper.ReadFromReplication(insertMessage, ct).ConfigureAwait(false);
-                await foreach (var subscribe in ProcessEnvelope(envelope, registry, errorProcessor).WithCancellation(ct).ConfigureAwait(false))
+                await foreach (var subscribe in ProcessEnvelope(envelope, registry, errorProcessor).WithCancellation(ct)
+                                   .ConfigureAwait(false))
                     yield return subscribe;
             }
+
             // Always call SetReplicationStatus() or assign LastAppliedLsn and LastFlushedLsn individually
             // so that Npgsql can inform the server which WAL files can be removed/recycled.
             _connection.SetReplicationStatus(message.WalEnd);
@@ -104,24 +118,15 @@ public sealed class Subscription: IAsyncDisposable
                 await errorProcessor.Process(error.Error).ConfigureAwait(false);
                 yield break;
             case OkEnvelope(var value, var messageType):
-            {
-                var objType = value.GetType();
-                    var (messageHandler, methodInfo) = _memoizer(messageType, registry[messageType].Item2, objType);
+                {
+                    var (messageHandler, methodInfo) =
+                        _messageHandler(messageType, registry[messageType].Item2, value.GetType());
                     await ((Task)methodInfo.Invoke(messageHandler, [value])!).ConfigureAwait(false);
 
                     yield return envelope;
-                yield break;
-            }
+                    yield break;
+                }
         }
-    }
-
-    private static (IMessageHandler messageHandler, MethodInfo methodInfo) MessageHandler(
-        string messageType, IMessageHandler messageHandler, Type objType)
-    {
-        var methodInfos = messageHandler.GetType().GetMethods(BindingFlags.Instance | BindingFlags.Public);
-        var methodInfo = methodInfos.SingleOrDefault(mi => mi.GetParameters().Any(pa => pa.ParameterType == objType))
-                         ?? throw new NotSupportedException($"Unregistered type for {objType.AssemblyQualifiedName}");
-        return (messageHandler, methodInfo);
     }
 
     private static async IAsyncEnumerable<IEnvelope> ReadExistingRowsFromSnapshot(
@@ -130,7 +135,7 @@ public sealed class Subscription: IAsyncDisposable
         ReplicationDataMapper dataMapper,
         TableDescriptorBuilder.MessageTable tableDescriptor,
         ISet<string> registeredTypes,
-    [EnumeratorCancellation] CancellationToken ct = default
+        [EnumeratorCancellation] CancellationToken ct = default
     )
     {
         var connection = await dataSource.OpenConnectionAsync(ct).ConfigureAwait(false);
@@ -146,7 +151,7 @@ public sealed class Subscription: IAsyncDisposable
 
     public async ValueTask DisposeAsync()
     {
-        if(_connection != null)
+        if (_connection != null)
             await _connection.DisposeAsync().ConfigureAwait(false);
     }
 }
