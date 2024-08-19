cea26de2df23207b602f1c396f84731fff5d1544
giordanol <luca.giordano@ynap.com>
2024-08-19 14:25:43 +0200

    default on not found

--- src/Blumchen/IDictionaryExtensions.cs
+++ src/Blumchen/IDictionaryExtensions.cs
@@ -2,7 +2,11 @@ namespace Blumchen;
 
 internal static class IDictionaryExtensions
 {
-    public static TR FindByMultiKey<T, TR>(this IDictionary<T, TR> registry, params T[] parameters)
-        where T : class =>
-        !registry.TryGetValue(parameters[0], out var value) ? registry.FindByMultiKey(parameters[1..parameters.Length]) : value;
+    public static TR? FindByMultiKey<T, TR>(this IDictionary<T, TR> registry, params T[] parameters) where T : class
+    {
+            if (parameters.Length == 0) return default;
+            return registry.TryGetValue(parameters[0], out var value)
+                ? value
+                : FindByMultiKey<T,TR>(registry, parameters[1..parameters.Length]);
+    }
 }
